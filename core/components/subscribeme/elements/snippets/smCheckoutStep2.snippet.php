33f12bd75cb986c66c237086ab74a0e7edc1527a
Mark-H <hamstra.mark@gmail.com>
2011-09-05 14:42:59 +0200

    Functional prior to splitting up in hooks

--- core/components/subscribeme/elements/snippets/smCheckoutStep2.snippet.php
+++ core/components/subscribeme/elements/snippets/smCheckoutStep2.snippet.php
@@ -2,38 +2,50 @@
 /* @var modX $modx
  * @var array $scriptProperties
  */
+// We don't want to use this anymore.
+return '';
+
 
 $path = $modx->getOption('subscribeme.core_path',null,$modx->getOption('core_path').'components/subscribeme/').'classes/';
 $modx->getService('sm','SubscribeMe',$path);
 require_once(dirname(dirname(dirname(__FILE__))).'/classes/paypal/paypal.class.php');
 
-$debug = true;
+$debug = $modx->getOption('debug',$scriptProperties,$modx->getOption('subscribeme.debug',null,false));
+$confirmAddress = $modx->getOption('confirmAddress',$scriptProperties,true);
+$tpl = $modx->getOption('tpl',$scriptProperties,'smcheckout.paymentoptions');
+$toPlaceholder = $modx->getOption('toPlaceholder',$scriptProperties,null);
 
 /* We will need to be logged in */
 if (!is_numeric($modx->user->id)) return $modx->sendUnauthorizedPage();
 
-/* Make sure we have a token and accompanying transaction ID */
+/* Make sure we have a token and accompanying subscription ID */
 $token = $modx->getOption('token',$_REQUEST);
 if (empty($token)) return 'Error, no token found.';
 
-/* @var smTransactionPaypal $pt */
-$pt = $modx->getObject('smTransactionPaypal',array('token' => $token));
-if (!($pt instanceof smTransactionPaypal)) return 'Error, no transaction token found.';
+/* @var smPaypalToken $pt */
+$pt = $modx->getObject('smPaypalToken',array('token' => $token));
+if (!($pt instanceof smPaypalToken)) return 'Error, no transaction token found.';
 
-/* @var smTransaction $transaction */
-$transaction = $pt->getOne('Transaction');
-if (!($transaction instanceof smTransaction)) return 'Error, no transaction found.';
-$transid = $transaction->get('trans_id');
+/* @var smSubscription $subscription */
+$subscription = $pt->getOne('Subscription');
+if (!($subscription instanceof smSubscription)) return 'Error, no subscription found.';
 
 /* Confirm valid transaction */
-if ($transaction->get('user_id') != $modx->user->id) { return 'Please make sure you are logged in with the user that requested this transaction.'; }
-if ($transaction->get('completed') === true) return 'Transaction already completed.';
+if ($subscription->get('user_id') != $modx->user->id) { return 'Please make sure you are logged in with the user that requested this subscription.'; }
+if ($subscription->get('active') === true) return 'Subscription already Active.';
+
+/* Prepare order / transaction data */
+$product = $subscription->getOne('Product');
+$profile = $modx->user->getOne('Profile');
+$user    = array_merge($profile->toArray(),$modx->user->toArray());
+$prod    = $product->toArray();
+$sub     = $subscription->toArray();
 
 /* If we passed the initial checks, let's move forward. */
 /* Prepare PayPal settings */
 $p = array();
 $p['currency_code'] = $modx->getOption('subscribeme.currencycode',null,'USD');
-$p['amount'] = $transaction->get('amount');
+$p['amount'] = $prod['price'];
 $p['return_url'] = $modx->makeUrl($modx->getOption('subscribeme.paypal.completed_id'), '', '', 'full');
 $p['cancel_url'] = $modx->makeUrl($modx->getOption('subscribeme.paypal.cancel_id'), '', array('transid' => $transid), 'full');
 $p['fail_id'] = $modx->getOption('subscribeme.paypal.fail_id');
@@ -60,112 +72,82 @@ $paypal->API_SIGNATURE = $p['signature'];
 
 $paypal->ip_address = $_SERVER['REMOTE_ADDR'];
 
-/* Prepare order / transaction data */
-$profile = $modx->user->getOne('Profile');
-$user    = array_merge($profile->toArray(),$modx->user->toArray());
-
-$subscription = $transaction->getOne('Subscription');
-$product      = $subscription->getOne('Product');
-
-$trans = $transaction->toArray();
-$prod  = $product->toArray();
-$sub   = $subscription->toArray();
-
 if ($debug) var_dump(array('PayPal Settings' => $p, 'User' => $user, 'Transaction' => $trans, 'Product' => $prod,'Subscription' => $sub));
 
 /* Start filling in some data */
 $paypal->version = '57.0';
-$paypal->token = $token;
-$paypal->invoice_number = $trans['id'];
-$paypal->currency = $p['currency_code'];
-$paypal->amount_total = $p['amount'];
-$paypal->email = $user['email'];
-$paypal->shipping_name = $user['fullname'];
-$paypal->shipping_address1 = $user['address'];
-$paypal->shipping_postal_code = $user['zip'];
-$paypal->shipping_state = $user['state'];
-$paypal->shipping_city = $user['city'];
-$paypal->shipping_country_name = $user['country'];
+$paypal->token = $_REQUEST['token'];
+
+/* Get the users details */
+$paypal->get_express_checkout_details();
+if ($debug) var_dump($paypal->Response);
+
+
+/* @TODO: Split it up here to allow a form where users need to confirm their shipping address & confirm the subscription */
 
 $paypal->return_url = $p['return_url'];
 $paypal->cancel_url = $p['cancel_url'];
 
-/*
- * 'description' 				=> array('name' => 'DESC',					'required' => 'yes'), // You must match the billing agreement var in set Express checkout
-    'currency'					=> array('name' => 'CURRENCYCODE',			'required' => 'yes'),
-    'payment_type' 				=> array('name' => 'PAYMENTACTION',			'required' => 'yes'),
-    'billing_type' 				=> array('name' => 'L_BILLINGTYPE0',		'required' => 'yes'),
-    'billing_agreement' 		=> array('name' => 'L_BILLINGAGREEMENTDESCRIPTION0', 'required' => 'yes'),
-    'profile_start_date'		=> array('name' => 'PROFILESTARTDATE',		'required' => 'yes'),
-    'billing_period' 			=> array('name' => 'BILLINGPERIOD',			'required' => 'yes'), // Day Week Month SemiMonth Year
-    'billing_frequency' 		=> array('name' => 'BILLINGFREQUENCY',		'required' => 'yes'),
-    'billing_amount' 			=> array('name' => 'AMT',					'required' => 'yes'),
-    'tax_amount' 				=> array('name' => 'TAXAMT',				'required' => 'yes'),
-    'ship_amount' 				=> array('name' => 'SHIPPINGAMT',			'required' => 'yes'),
-    'inital_amount' 			=> array('name' => 'INITAMT',				'required' => 'no'),
-    'failed_inital_amount' 		=> array('name' => 'FAILEDINITAMTACTION',	'required' => 'no'),
-    'billing_total_cycles' 		=> array('name' => 'TOTALBILLINGCYCLES',	'required' => 'no'),
-    'trial_billing_period' 		=> array('name' => 'TRIALBILLINGPERIOD',	'required' => 'no'),
-    'trial_billing_frequency' 	=> array('name' => 'TRIALBILLINGFREQUENCY',	'required' => 'no'),
-    'trial_amount' 				=> array('name' => 'TRIALAMT', 				'required' => 'no'),
-    'trial_billing_cycle' 		=> array('name' => 'TRIALTOTALBILLINGCYCLES', 'required' => 'no'),
-    'max_failed_attempts'  		=> array('name' => 'MAXFAILEDPAYMENTS', 	'required' => 'no'),
-    'auto_bill_amt'  			=> array('name' => 'AUTOBILLOUTAMT', 		'required' => 'no')
- */
-
+/* Set recurring payment information */
 $start_time = strtotime(date('m/d/Y'));
 $start_date = gmdate('Y-m-d\T00:00:00\Z',$start_time);
+$paypal->profile_start_date = urlencode($start_date);
 
-$paypal->profile_start_date = gmdate('d-m-Y\TH:i:s\Z');// '2011-09-05T05:00:00.0000000Z'; //$start_date;//'2011-09-04 T01:09:14Z%20'; //date('Y-m-d\T+H:i:s\Z ',strtotime('+1month'));
+$paypal->invoice_number = $trans['id'];
+$paypal->currency = $p['currency_code'];
 $paypal->billing_start = gmdate('d-m-Y\TH:i:s\Z');// '2011-09-05T05:00:00.0000000Z'; //$start_date;//'2011-09-04 T01:09:14Z%20'; //date('Y-m-d\T+H:i:s\Z ',strtotime('+1month'));
-$periodUsable = array('D' => 'Day', 'W' => 'Week', 'M' => 'Month', 'Y' => 'Year');
-$paypal->billing_period = $periodUsable[$prod['period']];
+$paypal->billing_period = ucfirst($modx->sm->periodUsable[$prod['period']]);
 $paypal->billing_frequency = $prod['periods'];
 $paypal->billing_amount = $prod['price'];
 $paypal->billing_type = 'RecurringPayments';
-$paypal->billing_agreement = 'Subscription #'.$sub['sub_id'].' for '.$prod['name'];
-$paypal->description = 'Subscription #'.$sub['sub_id'].' for '.$prod['name'];
-$paypal->profile_reference = $user['id'];
+$paypal->billing_type2 = 'RecurringPayments';
+$paypal->billing_agreement = urlencode($prod['name'].' (#'.$sub['sub_id'].')');
+$paypal->description = urlencode($prod['name'].' (#'.$sub['sub_id'].')');
+$paypal->profile_reference = $sub['sub_id'];
 $paypal->payer_id = $_REQUEST['PayerID'];
-$paypal->tax_amount = 5.12;
-$paypal->ship_amount = 1.32;
-$paypal->subscriber_name = $user['fullname'];
+$paypal->tax_amount = 0.00;
+$paypal->ship_amount = 0.00;
+$paypal->subscriber_name = urlencode($user['fullname']);
 
-$country_array = preg_grep('/^'.$user['country'].'/', $paypal->countries);
-foreach ($country_array as $key => $value) {
-    $ta[] = $key;
-}
-$paypal->shipping_country_code = $ta[0];
-//https://api-3t.sandbox.paypal.com/nvp?USER=spiral_1241371009_biz_api1.gmail.com&PWD=1241371016&SIGNATURE=ABUaThl3V14Xe5aZuHM17VrRhoaQAgCa6gxyRuUEJDM41CW9AmXbhtth&VERSION=53.0&METHOD=CreateRecurringPaymentsProfile&TOTALBILLINGCYCLES=2&BILLINGPERIOD=Month&COUNTRYCODE=US&CREDITCARDTYPE=Visa&ZIP=95131&FIRSTNAME=John&EXPDATE=012010&PROFILESTARTDATE=2009-05-022T05:00:00.0000000Z&CITY=Omaha&AUTOBILLOUTAMT=NoAutoBill&STREET=123+Fake+St&ACCT=4560151348340840&DESC=Test&LASTNAME=Doe&AMT=1.00&BILLINGFREQUENCY=1&STATE=CA&CVV2=962&INITAMT=1.00&FAILEDINITAMTACTION=CancelOnFailure&PAYMENTACTION=sale&CURRENCYCODE=USD
-
-//$paypal->get_express_checkout_details();
-
+/* Create the profile */
 $paypal->create_recurring_payments_profile();
 
 $response = $paypal->Response;
 $success = false;
 
-if (isset($response['PAYMENTSTATUS'])) {
-    switch (strtolower($response['PAYMENTSTATUS'])) {
-        case 'completed':
+if (isset($response['PROFILESTATUS'])) {
+    switch (strtolower($response['PROFILESTATUS'])) {
+        case 'activeprofile':
             $success = true;
-            echo 'Completed';
-            var_dump($response);
+            /* We succesfully set up the recurring payments profile! PARTY!!!
+             * Do note that that doesn't mean we got money yet - we'll need to wait for the IPN message for that.
+             * We will, however, set the PROFILEID to the subscription so we can identify it and use that to fetch info.
+             */
+            $subscription->set('pp_profileid',$response['PROFILEID']);
+                /* @todo: mark as active */
+            if ($subscription->save())
+                echo 'Subscription set up properly!';
+            if ($debug) var_dump($response);
             break;
-        case 'pending':
+        
+        case 'pendingprofile':
             $success = true;
-            echo 'Pending';
-            var_dump($response);
+            $subscription->set('pp_profileid',$response['PROFILEID']);
+            if ($subscription->save())
+                echo 'Subscription set up, but currently pending activation.';
+            if ($debug) var_dump($response);
             break;
+
         default:
-            echo 'Dunno';
-            var_dump($response);
+            echo 'Something unexpected happened.';
+            if ($debug) var_dump($response);
             break;
     }
 }
 else {
     // Uh oh.. trouble!
-    var_dump($response);
+    if ($debug) var_dump($response);
+    echo 'Something went wrong creating recurring payments profile.';
 }
 
 ?>
